ba83884c75d6484cd3ac841c84a8ee84e84bed96
Ryan.Desktop <ryan@RyanL-Desktop.localdomain>
2020-03-24 10:32:04 +0100

    Ecoute sur deux ports

--- Exercice 4/serveur-v3.c
+++ Exercice 4/serveur-v3.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <sys/select.h>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +14,10 @@ int main(int argc, char *argv[])
     int sock_log, res_log;
     struct sockaddr_in add_web;
     struct sockaddr_in add_log;
+    int sock_web_service;
+    int sock_log_service;
+    socklen_t taille_web = sizeof(add_web);
+    socklen_t taille_log = sizeof(add_log);
 
     sock_web = socket(AF_INET, SOCK_STREAM, 0);
     sock_log = socket(AF_INET, SOCK_STREAM, 0);
@@ -52,6 +57,47 @@ int main(int argc, char *argv[])
     {
         printf("Error bind");
         exit(2);
+    }
+
+    fd_set groupe1;
+    
+    while (1)
+    {
+        FD_ZERO(&groupe1); /*initialise le groupe1*/
+        FD_SET(sock_web, &groupe1);
+        FD_SET(sock_log, &groupe1);
+        select(sock_log + 1, &groupe1, NULL, NULL, 0);
+
+        if (FD_ISSET(sock_web, &groupe1) == 1)
+        {
+            sock_web_service = accept(sock_web, (struct sockaddr *)&add_web, (socklen_t *)&taille_web);
+
+            if (sock_web_service < 0)
+            {
+                printf("Error accept");
+                close(sock_web_service);
+                exit(3);
+            }
+            else
+            {
+                read(sock_web_service, &groupe1, sizeof(groupe1));
+            }
+        }
+        if (FD_ISSET(sock_log, &groupe1) == 1)
+        {
+            sock_log_service = accept(sock_log, (struct sockaddr *)&add_log, (socklen_t *)&taille_log);
+
+            if (sock_log_service < 0)
+            {
+                printf("Error accept");
+                close(sock_log_service);
+                exit(3);
+            }
+            else
+            {
+                read(sock_log_service, &groupe1, sizeof(groupe1));
+            }
+        }
     }
 
     return 0;
